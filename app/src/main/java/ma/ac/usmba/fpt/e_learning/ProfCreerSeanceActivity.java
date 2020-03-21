6534d9828a8225d4928135a945ed378b96377537
elaissaoui-ali-sim <ali_elaissaoui@live.fr>
2020-03-21 19:00:38 +0100

    ProfLoginValidiation2

--- app/src/main/java/ma/ac/usmba/fpt/e_learning/ProfCreerSeanceActivity.java
+++ app/src/main/java/ma/ac/usmba/fpt/e_learning/ProfCreerSeanceActivity.java
@@ -1,57 +1,16 @@
 package ma.ac.usmba.fpt.e_learning;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.FileUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class ProfCreerSeanceActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    Intent intent;
-    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prof_creer_seance);
-        Button creer_seance = (Button) findViewById(R.id.btn_creer_quiz);
-        creer_seance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ProfCreerSeanceActivity.this,QuizPopUp.class);
-                startActivity(intent);
-            }
-        });
-        textView = (TextView) findViewById(R.id.file_name);
-        ImageView imageView = (ImageView) findViewById(R.id.image_view_attachement);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("*/*");
-            startActivityForResult(intent,10);
-            }
-        });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 10:
-                if (resultCode == RESULT_OK) {
-                    String path = data.getData().getPath();
-                    System.out.println(path);
-                    textView.setText(path);
-                }
-                break;
-        }
+
     }
 }
