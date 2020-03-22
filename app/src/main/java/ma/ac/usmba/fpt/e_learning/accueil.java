20e34f35bd47bb283a0c275fcd207a826581a8e7
elbouni fati <fatimazahra.elbouni@usmba.ac.ma>
2020-03-22 13:11:14 +0100

    fati

---
+++ app/src/main/java/ma/ac/usmba/fpt/e_learning/accueil.java
@@ -0,0 +1,14 @@
+package ma.ac.usmba.fpt.e_learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+    }
+}
