b41b43551c7ba888171d65e06d8eb07d3c57e0c4
elaissaoui-ali-sim <ali_elaissaoui@live.fr>
2020-04-25 02:59:05 +0000

    The first steps to create a quiz area for the prof to control the answers of student

--- app/src/main/java/ma/ac/usmba/fpt/e_learning/ProfSeanceQuizFrag.java
+++ app/src/main/java/ma/ac/usmba/fpt/e_learning/ProfSeanceQuizFrag.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,12 @@ public class ProfSeanceQuizFrag extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_prof_seance_quiz, container, false);
+
+        TextView name = view.findViewById(R.id.name);
+        name.setText("Ali Elaissaoui");
+        TextView score = view.findViewById(R.id.score_text);
+        score.setText("5/6");
+
         return view;
     }
 }
